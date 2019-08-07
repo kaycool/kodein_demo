57d13d377737b979732c8e4a7d66fe397ad88daa
wangkai <wk343321@126.com>
2019-08-07 21:37:39 +0800

    first commit

---
+++ app/src/main/java/com/kai/wang/kodein_demo/App.kt
@@ -0,0 +1,14 @@
+package com.kai.wang.kodein_demo
+
+import com.sirit.android.support.lib.base.BaseApp
+
+/**
+ * @author kai.w
+ *      ${DES}
+ */
+class App : BaseApp() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
